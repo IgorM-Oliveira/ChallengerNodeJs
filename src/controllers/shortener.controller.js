753db087cc18554e985c9c68aaa7dd7e499e100c
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2021-12-28 18:39:04 -0400

    add generate code, link with mongoDb and initial update

--- src/controllers/shortener.controller.js
+++ src/controllers/shortener.controller.js
@@ -1,13 +1,70 @@
-// const Shortener = require('../models/shortener.model');
+const Shortener = require('../models/shortener.model');
+const User = require("../models/user.model");
+
+function generateCode() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i=0; i<5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return 'http://localhost:3000/' + text
+}
+
+exports.shortness = async (req, res) => {
+    try {
+        const links = await Shortener.find();
+        return res.send({links})
+    } catch (err) {
+        return res.status(400).json({message: "Não foi possível realizar está ação!"});
+    }
+}
 
 exports.shortener = async (req, res) => {
     try {
-        // const newShortener = new Shortener(req.body);
-        // const user = await newShortener.save();
+        const isUrl = await Shortener.find({url: req.body.url});
+
+        if (isUrl.length >= 1) {
+            // await updateUrl({isUrl})
+            return res
+                .status(409)
+                .json({message: "Atenção! Este URL já possui registro!"});
+        }
+
+        const newShortener = new Shortener({
+            url: req.body.url,
+            user: req.body.user,
+            code: generateCode(),
+            hits: 1
+        });
+
+        const shortener = await newShortener.save();
         return res
             .status(201)
-            .json({message: "URL encurtada com sucesso!"});
+            .json({
+                message: "URL encurtada com sucesso!",
+                shortener: shortener.code
+            })
+        } catch (err) {
+        return res.status(400).json({message: "Não foi possível realizar está ação!"});
+    }
+};
+
+const updateUrl = async (req, res) => {
+    try {
+        const {email} = req.body;
+        const {password} = req.body;
+        const user = await User.findByCredentials(email, password);
+        if (!user) {
+            return res.status(401).json({
+                error: "Erro ao Logar! Verifique as suas credenciais de autenticação!",
+            });
+        }
+        const token = await user.generateAuthToken();
+        return res
+            .status(201)
+            .json({message: "Usuário(a) logado com sucesso!", user, token});
     } catch (err) {
-        return res.status(400).json({err});
+        return res.status(400).json({message: "Não foi possível realizar está ação!"});
     }
 };
