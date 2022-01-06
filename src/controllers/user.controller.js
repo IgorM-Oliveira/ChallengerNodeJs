e76b1ce3665b9ffa88d44c35b5d6801c63891855
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2022-01-06 16:37:56 -0400

    bank switch, local to cloud and initial user anonymous

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,8 +30,59 @@ exports.userRegistration = async (req, res) => {
     }
 };
 
+exports.userRegistrationAnonymous = async (req, res) => {
+    try {
+        const isUser = await User.find({email: 'anonymity@gmail.com'});
+
+        if (isUser.length >= 1) {
+            return res
+                .status(409)
+                .json({message: "Atenção! Este e-mail já possui registro!"});
+        }
+
+        const user = await User.create(
+            {
+                "name": "Anônimo",
+                "email": "anonymity@gmail.com",
+                "password": "anonymous"
+            }
+        );
+
+        user.password = undefined;
+
+        return res.send({ user, token: generateToken({ id: user._id }) });
+    } catch (err) {
+        return res.status(400).json({err});
+    }
+};
+
 exports.userAuthenticate = async (req, res) => {
     const {email, password} = req.body;
+
+    const user = await User.findOne({email}).select('+password');
+
+    if (!user) {
+        return res
+            .status(400)
+            .json({message: "Usuário(a) não encontrado"});
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+        return res
+            .status(400)
+            .json({message: "Senha invalida"});
+    }
+
+    user.password = undefined;
+
+    res.send({ user, token: generateToken({ id: user._id }) });
+};
+
+exports.userAuthenticateAnonymous = async (req, res) => {
+    const {email, password} = {
+        "email": "anonymous@gmail.com",
+        "password": "anonymous"
+    };
 
     const user = await User.findOne({email}).select('+password');
 
