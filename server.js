94dd065a53c2e4cdff2bec1bcb99ff914c87d903
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2022-01-08 12:56:25 -0400

    add server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const app = require('./src/app');
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+
+const app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 const port = process.env.PORT || 3000;
 
